f6ad8cee9ffc6200854bb87f4aca5d25229f9d23
OliverDanger <od.oliverdixon@gmail.com>
2023-06-16 21:23:06 +0000

    create owned recipes and recipes table migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_202808) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_212051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_202808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_owned_articles_on_user_id"
+  end
+
+  create_table "owned_recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_owned_recipes_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.text "description"
+    t.boolean "consumable", default: false
+    t.integer "item_1"
+    t.integer "item_2"
+    t.integer "item_3"
+    t.integer "item_4"
+    t.integer "item_5"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
