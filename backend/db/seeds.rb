8cb1570b22bce2c94057a3e7f91ff0b3d9694efa
OliverDanger <od.oliverdixon@gmail.com>
2023-06-16 17:15:00 +0000

    add migration for inventories and update seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -25,43 +25,45 @@ puts "Creating Items..."
 
 Item.destroy_all
 
-item1 = Item.create!({
-  name: "Tuna",
-  description: "Classic fish. Tuna can be found anywhere in the bay.",
-  melting_point: 35.00,
-  base_value: 25.00,
-  weight: 3.21,
-  magnetic: false,
-  hookable: true,
-  netable: true,
-})
-item2 = Item.create!({
-  name: "Sea Snail",
-  description: "They live everywhere and eat everything, some say they even eat eachother!",
-  melting_point: 40.00,
-  base_value: 0.70,
-  weight: 0.25,
-  magnetic: false,
-  hookable: false,
-  netable: true,
-})
-item3 = Item.create!({
-  name: "Empty Can",
-  description: "An empty can with a worn fishcorp label. The ocean has cleaned it up nicely. It's made of tin but is strangely magnetic...",
-  melting_point: 660.32,
-  base_value: 0.15,
-  weight: 0.66,
-  magnetic: true,
-  hookable: false,
-  netable: true,
-})
-item4 = Item.create!({
-  name: "Empty Water Bottle",
-  description: "An empty water bottle with a worn fishcorp label. The ocean has cleaned it up nicely. It's made of low quality plastic.",
-  melting_point: 255.55,
-  base_value: 0.10,
-  weight: 0.33,
-  magnetic: false,
-  hookable: false,
-  netable: true,
-})
+items = Item.create!([
+  {
+    name: "Tuna",
+    description: "Classic fish. Tuna can be found anywhere in the bay.",
+    melting_point: 35.00,
+    base_value: 25.00,
+    weight: 3.21,
+    magnetic: false,
+    hookable: true,
+    netable: true,
+  },
+  {
+    name: "Sea Snail",
+    description: "They live everywhere and eat everything, some say they even eat eachother!",
+    melting_point: 40.00,
+    base_value: 0.70,
+    weight: 0.25,
+    magnetic: false,
+    hookable: false,
+    netable: true,
+  },
+  {
+    name: "Empty Can",
+    description: "An empty can with a worn fishcorp label. The ocean has cleaned it up nicely. It's made of tin but is strangely magnetic...",
+    melting_point: 660.32,
+    base_value: 0.15,
+    weight: 0.66,
+    magnetic: true,
+    hookable: false,
+    netable: true,
+  },
+  {
+    name: "Empty Water Bottle",
+    description: "An empty water bottle with a worn fishcorp label. The ocean has cleaned it up nicely. It's made of low quality plastic.",
+    melting_point: 255.55,
+    base_value: 0.10,
+    weight: 0.33,
+    magnetic: false,
+    hookable: false,
+    netable: true,
+  }
+])
