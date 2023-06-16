f3b10b105f0650d72786b35c0980291ac9605a70
OliverDanger <od.oliverdixon@gmail.com>
2023-06-16 19:40:05 +0000

    create users migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_170705) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_190904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_170705) do
     t.boolean "magnetic", default: false
     t.boolean "hookable", default: true
     t.boolean "netable", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
