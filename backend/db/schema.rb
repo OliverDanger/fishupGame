e89386925a63f9267c528f10e173674408162ed6
OliverDanger <od.oliverdixon@gmail.com>
2023-06-16 20:30:31 +0000

    create migration for owned articles of clothing

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_201543) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_202808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_201543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_outfits_on_user_id"
+  end
+
+  create_table "owned_articles", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "clothing_article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_owned_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
