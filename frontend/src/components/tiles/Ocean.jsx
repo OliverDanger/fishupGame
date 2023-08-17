e609b90dabf67bed3913533c2fe1cc7a7c9e6e7b
OliverDanger <od.oliverdixon@gmail.com>
2023-08-17 16:37:20 -0400

    add Ocean and Sand tile component files

---
+++ frontend/src/components/tiles/Ocean.jsx
@@ -0,0 +1,9 @@
+function Ocean() {
+  return (
+    <div className="Ocean">
+      <h1>Ocean</h1>
+    </div>
+  );
+}
+
+export default Ocean;
---
+++ frontend/src/components/tiles/Sand.jsx
@@ -0,0 +1,9 @@
+function Sand() {
+  return (
+    <div className="Sand">
+      <h1>Sand</h1>
+    </div>
+  );
+}
+
+export default Sand;
