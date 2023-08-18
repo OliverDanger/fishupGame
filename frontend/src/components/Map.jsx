3b92267b5682ba2aaff39263ee51aefca5ff195c
OliverDanger <od.oliverdixon@gmail.com>
2023-08-18 16:18:28 -0400

    Map component fetches data from the API using axios and displays it

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,46 +1,12 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 
 import Ocean from "./tiles/Ocean";
 import Sand from "./tiles/Sand";
 
 import "../styles/Map.scss";
 
-const mapJSON =
-  [
-    {
-      id: 9,
-      x: 0,
-      y: 0,
-      type_number: 0,
-      created_at: "2023-07-19T20:27:36.725Z",
-      updated_at: "2023-07-19T20:27:36.725Z"
-    },
-    {
-      id: 10,
-      x: 1,
-      y: 0,
-      type_number: 0,
-      created_at: "2023-07-19T20:27:36.732Z",
-      updated_at: "2023-07-19T20:27:36.732Z"
-    },
-    {
-      id: 11,
-      x: 0,
-      y: 1,
-      type_number: 0,
-      created_at: "2023-07-19T20:27:36.737Z",
-      updated_at: "2023-07-19T20:27:36.737Z"
-    },
-    {
-      id: 12,
-      x: 1,
-      y: 1,
-      type_number: 1,
-      created_at: "2023-07-19T20:27:36.744Z",
-      updated_at: "2023-07-19T20:27:36.744Z"
-    }
-  ];
+
 
 // all tile component names must be added here
 const tileComponents = {
@@ -49,15 +15,28 @@ const tileComponents = {
 };
 
 const Map = function() {
+
+  const [mapData, setMapData] = useState([]);
+  //get user's map data from the server
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/tiles")
+      .then(res => {
+        setMapData(res.data);
+      })
+      .catch(error => {
+        console.error("Error fetching map data:", error);
+      });
+  }, []);
+
   // Find the maximum x and y values to determine grid size
-  const maxX = Math.max(...mapJSON.map(tile => tile.x)) + 1;
-  const maxY = Math.max(...mapJSON.map(tile => tile.y)) + 1;
+  const maxX = Math.max(...mapData.map(tile => tile.x)) + 1;
+  const maxY = Math.max(...mapData.map(tile => tile.y)) + 1;
 
   // Create a 2D array to represent the grid
   const grid = Array.from({ length: maxY }, () => Array(maxX).fill(null));
 
   // Populate the grid with tile components
-  mapJSON.forEach(tile => {
+  mapData.forEach(tile => {
     const { x, y, type_number } = tile;
     grid[y][x] = tileComponents[type_number];
   });
