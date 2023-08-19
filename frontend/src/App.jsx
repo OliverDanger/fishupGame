c604e8844080d24d5640c6ec973e40a2262af259
OliverDanger <od.oliverdixon@gmail.com>
2023-08-18 23:45:11 -0400

    setup route for MapView

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 //components
-import Map from './components/Map';
+import MapView from './components/MapView';
 
 const App = () => {
   return (
-    <>
-      <Map />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/map" element={<MapView />} />
+      </Routes>
+    </Router>
   );
 };
 
