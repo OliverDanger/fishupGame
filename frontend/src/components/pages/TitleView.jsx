d9245856f9fe29f8e352f8f5c27fadfa2424c050
OliverDanger <od.oliverdixon@gmail.com>
2023-08-20 20:49:15 -0400

    add link from title to map page and style title card

--- frontend/src/components/pages/TitleView.jsx
+++ frontend/src/components/pages/TitleView.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import "../../styles/TitleView.scss";
 
 const TitleView = () => {
   return (
     <div className="title-view">
       <header></header>
-      <section className="title-card">
-        <h1>Fish-Up Game</h1>
-        <p>by Oliver Danger Dixon</p>
-      </section>
+      <div className="background-container"> {/* Extra container for background separation */}
+        <section className="title-card">
+          <h1>Fish-Up Game</h1>
+          <p>by Oliver Danger Dixon</p>
+          <Link to={'/map'}>
+            <button>Map</button>
+          </Link>
+        </section>
+      </div>
     </div>
   );
 };
