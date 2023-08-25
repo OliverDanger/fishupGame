a9b2db7e35a946643a9ed92f2bb24d9d194b5b7b
OliverDanger <od.oliverdixon@gmail.com>
2023-08-25 13:37:10 -0400

    create state for player position

--- frontend/src/components/pages/MapView.jsx
+++ frontend/src/components/pages/MapView.jsx
@@ -1,15 +1,15 @@
 import Map from "../Map";
 import "../../styles/pages/MapView.scss";
+import { useState } from "react";
 
 const MapView = function() {
+
+  const [playerPosition, setPlayerPosition] = useState({x: 0, y: 0});
+
   return (
     <div className="map-view">
-      <div className="map-container">
-        <div className="player"></div>
-        <div className="map-content">
-          <Map />
-        </div>
-      </div>
+      <div className="player"></div>
+      <Map playerPosition={playerPosition}/>
     </div>
   );
 };
