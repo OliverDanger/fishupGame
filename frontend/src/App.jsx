f40639a35c893e50193d58245dad0fa9f70b3cdd
OliverDanger <od.oliverdixon@gmail.com>
2023-09-14 21:45:54 -0400

    create routes for below deck and cabin pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //components
 import TitleView from './components/pages/TitleView';
 import MapView from './components/pages/MapView';
+import CabinView from './components/pages/CabinView';
+import BelowDeckView from './components/pages/BelowDeckView';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<TitleView />} />
         <Route path="/map" element={<MapView />} />
+        <Route path="/cabin" element={<CabinView />} />
+        <Route path='/below' element={<BelowDeckView />} />
       </Routes>
     </Router>
   );
