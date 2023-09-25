8c1d33d36b1175e0f8d9c57be12d1bc1960dd73c
OliverDanger <od.oliverdixon@gmail.com>
2023-09-25 18:17:48 -0400

    simplify and clean up map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -6,29 +6,15 @@ import Sand from "./tiles/Sand";
 
 import "../styles/Map.scss";
 
-
-
-// Constants
-
 const tileSize = 100;
 
-// all tile component names must be named aand numbered here
 const tileComponents = {
   0: Ocean,
   1: Sand,
 };
 
-const Map = function({ windowDimensions }) {
-
+const Map = function ({ windowDimensions }) {
   const [mapData, setMapData] = useState([]);
-  const [targetTile, setTargetTile] = useState({ x: 12, y: 12 });
-  // Set the initial scroll position based on the targetTile
-  const initialScrollLeft = targetTile.x * tileSize;
-  const initialScrollTop = targetTile.y * tileSize;
-  const [scrollPosition, setScrollPosition] = useState({ scrollLeft: initialScrollLeft, scrollTop: initialScrollTop });
-
-  //get user's map data from the server
-  //temp location
 
   useEffect(() => {
     axios.get("http://localhost:3001/api/tiles")
@@ -40,14 +26,11 @@ const Map = function({ windowDimensions }) {
       });
   }, []);
 
-  // Find the maximum x and y values to determine grid size
-  const maxX = Math.max(...mapData.map(tile => tile.x)) + 1;
-  const maxY = Math.max(...mapData.map(tile => tile.y)) + 1;
+  const maxXCoordinate = Math.max(...mapData.map(tile => tile.x)) + 1;
+  const maxYCoordinate = Math.max(...mapData.map(tile => tile.y)) + 1;
 
-  // Create a 2D array to represent the grid
-  const grid = Array.from({ length: maxY }, () => Array(maxX).fill(null));
+  const grid = Array.from({ length: maxYCoordinate }, () => Array(maxXCoordinate).fill(null));
 
-  // Populate the grid with tile components
   mapData.forEach(tile => {
     const { x, y, type_number } = tile;
     grid[y][x] = tileComponents[type_number];
@@ -56,31 +39,12 @@ const Map = function({ windowDimensions }) {
   return (
     <div className="map">
       <Grid
-        useIsScrolling={false}
-        columnCount={maxX}
-        rowCount={maxY}
+        columnCount={maxXCoordinate}
+        rowCount={maxYCoordinate}
         columnWidth={tileSize}
         rowHeight={tileSize}
         height={windowDimensions.height - 25}
         width={windowDimensions.width - 25}
-        onItemsRendered={({ visibleColumnStartIndex, visibleRowStartIndex }) => {
-          // Calculate the center tile coordinates
-          const centerX = Math.floor(maxX / 2);
-          const centerY = Math.floor(maxY / 2);
-
-
-          // Calculate the new scroll position based on the targetTile coordinates
-          const newScrollLeft = targetTile.x * tileSize;
-          const newScrollTop = targetTile.y * tileSize;
-
-          // Only update the scroll position if it has changed
-          if (newScrollLeft !== scrollPosition.scrollLeft || newScrollTop !== scrollPosition.scrollTop) {
-            setScrollPosition({ scrollLeft: newScrollLeft, scrollTop: newScrollTop });
-          }
-        }}
-        // Set the initial scroll position
-        scrollLeft={scrollPosition.scrollLeft}
-        scrollTop={scrollPosition.scrollTop}
       >
         {({ columnIndex, rowIndex, style }) => {
           const TileComponent = grid[rowIndex][columnIndex];
@@ -90,22 +54,16 @@ const Map = function({ windowDimensions }) {
               style={style}
               key={`${rowIndex}-${columnIndex}`}
               onClick={() => {
-                // Calculate new scroll position based on clicked tile's coordinates
-                const newScrollLeft = columnIndex * tileSize;
-                const newScrollTop = rowIndex * tileSize;
-
-                // Set the new scroll position
-                setScrollPosition({ scrollLeft: newScrollLeft, scrollTop: newScrollTop });
-                console.log('✨', rowIndex, columnIndex);
-                console.log('🐟', scrollPosition);
+                // Your click handling code can go here
+                console.log('Tile clicked at:', rowIndex, columnIndex);
               }}
             >
               {TileComponent && (<TileComponent x={rowIndex} y={columnIndex} />)}
             </div>
           );
         }}
-      </Grid >
-    </div >
+      </Grid>
+    </div>
   );
 };
 
