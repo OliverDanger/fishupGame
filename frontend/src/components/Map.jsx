fb4e764ba4b2a191018825ae1c5f7a9830c17816
OliverDanger <od.oliverdixon@gmail.com>
2023-09-29 16:11:56 -0400

    arrow keys control focusedTile state in map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,14 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { FixedSizeGrid as Grid } from "react-window";
-import {getTileComponent} from '../utils/TileList.jsx';
+import { getTileComponent } from '../utils/TileList';
+import { tileSize, mapWindowMargin } from "../utils/_constants";
 
 import "../styles/Map.scss";
 
-const tileSize = 100;
 
-const Map = function ({ windowDimensions }) {
+const Map = function({ windowDimensions }) {
   const [mapData, setMapData] = useState([]);
+  const [focusedTile, setFocusedTile] = useState({ x: 10, y: 10 });
+  const gridRef = useRef(null);
 
   useEffect(() => {
     axios.get("http://localhost:3001/api/tiles")
@@ -20,37 +22,81 @@ const Map = function ({ windowDimensions }) {
       });
   }, []);
 
+  // calculate the max dimensions of the current map, and create 2d matrix of arrays to match
   const maxXCoordinate = Math.max(...mapData.map(tile => tile.x)) + 1;
   const maxYCoordinate = Math.max(...mapData.map(tile => tile.y)) + 1;
-
   const grid = Array.from({ length: maxYCoordinate }, () => Array(maxXCoordinate).fill(null));
 
+  // map the map data into the grid
   mapData.forEach(tile => {
     const { x, y, type_number } = tile;
     grid[y][x] = getTileComponent(type_number);
   });
 
+  // function to handle the key presses
+  const handleArrowKey = (event) => {
+    let newX = focusedTile.x;
+    let newY = focusedTile.y;
+
+    switch (event.key) {
+      case "ArrowLeft":
+        newX--;
+        console.log('left!');
+        break;
+      case "ArrowRight":
+        newX++;
+        break;
+      case "ArrowUp":
+        newY++;
+        break;
+      case "ArrowDown":
+        newY--;
+        break;
+      default:
+        return;
+    }
+
+    setFocusedTile({ x: newX, y: newY });
+    console.log('✨', focusedTile);
+
+    // Scroll to the new focused tile
+    gridRef.current.scrollToItem({
+      columnIndex: newX,
+      rowIndex: newY,
+      align: "center",
+    });
+  };
+
+  useEffect(() => {
+    // Add event listener for key presses
+    window.addEventListener("keydown", handleArrowKey);
+
+    return () => {
+      // remove the event listener when the component unmounts
+      window.removeEventListener("keydown", handleArrowKey);
+    };
+  }, [focusedTile]);
+
   return (
     <div className="map">
       <Grid
+        ref={gridRef}
         columnCount={maxXCoordinate}
         rowCount={maxYCoordinate}
         columnWidth={tileSize}
         rowHeight={tileSize}
-        height={windowDimensions.height - 25}
-        width={windowDimensions.width - 25}
+        height={windowDimensions.height - mapWindowMargin}
+        width={windowDimensions.width - mapWindowMargin}
+        initialScrollLeft={focusedTile.x * tileSize}
+        initialScrollTop={focusedTile.y * tileSize}
       >
         {({ columnIndex, rowIndex, style }) => {
           const TileComponent = grid[rowIndex][columnIndex];
           return (
             <div
-              className="mapTile"
+              className={`mapTile ${focusedTile.x === columnIndex && focusedTile.y === rowIndex ? 'focused' : ''}`}
               style={style}
               key={`${rowIndex}-${columnIndex}`}
-              onClick={() => {
-                // Your click handling code can go here
-                console.log('Tile clicked at:', rowIndex, columnIndex);
-              }}
             >
               {TileComponent && (<TileComponent x={rowIndex} y={columnIndex} />)}
             </div>
---
+++ frontend/src/utils/_constants.js
@@ -0,0 +1,4 @@
+// Map Constants
+
+export const tileSize = 100;
+export const mapWindowMargin = 25;
