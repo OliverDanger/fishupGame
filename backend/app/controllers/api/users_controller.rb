0b0495e585240c608eba41665fcd2818eac23c5f
OliverDanger <od.oliverdixon@gmail.com>
2023-10-24 17:23:29 -0400

    Add UsersController#get_user_info action to retrieve user information by username

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def get_user_info
+    @user = User.find_by(username: params[:username])
+
+    if @user
+      render json: @user
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
 end
