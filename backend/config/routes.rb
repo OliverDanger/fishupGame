72644730354ca60d3b230f10f8bb601a3da1512d
OliverDanger <od.oliverdixon@gmail.com>
2023-10-24 18:53:56 -0400

    create route for user owned clothing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  namespace :api do
-    defaults format: :json do
-
-      resources :tiles, only: [:index]
-      resources :clothing_articles, only: [:index]
-      resources :users, only: [] do 
-        collection do
-          get 'get_user_info'
-        end
+  namespace :api, defaults: { format: :json } do
+    resources :tiles, only: [:index]
+    resources :clothing_articles, only: [:index]
+    resources :users, only: [] do 
+      collection do
+        get 'get_user_info'
       end
-
     end
+    resources :owned_articles, only: [:index]
+
+    # Custom route for the 'index' action of the OwnedArticlesController
+    get 'users/:user_id/owned_clothing', to: 'owned_articles#index', as: 'user_owned_clothing'
   end
 end
