d8c097095fa4ab7395be6854faebbb59b01ead1f
OliverDanger <od.oliverdixon@gmail.com>
2023-10-25 00:33:06 -0400

    closet view displays users clothes provided by user context

--- frontend/src/components/pages/ClosetView.jsx
+++ frontend/src/components/pages/ClosetView.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 
 import "../../styles/pages/ClosetView.scss";
 
 const ClosetView = () => {
+
+  const { userClothes } = useContext(UserContext);
   
   return (
     <div className="closet-view">
       <h1>Closet</h1>
+      <ul>
+        {userClothes.map((clothing, index) => (
+          <li key={index}>
+            <p>{`ID: ${clothing.id}, User ID: ${clothing.user_id}, Clothing ID: ${clothing.clothing_article_id}`}</p>
+            <p>{`Created at: ${clothing.created_at}, Updated at: ${clothing.updated_at}`}</p>
+          </li>
+        ))}
+      </ul>
       <Link to={'/cabin'}>
         <button>Back</button>
       </Link>
