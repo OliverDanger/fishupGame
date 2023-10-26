1d9d2a6015e96be6caa157c62673630f929370e7
OliverDanger <od.oliverdixon@gmail.com>
2023-10-25 20:08:42 -0400

    display pose 1 png on closet page

--- frontend/src/components/pages/ClosetView.jsx
+++ frontend/src/components/pages/ClosetView.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
-
 import "../../styles/pages/ClosetView.scss";
+
+import paperdoll from "../../assets/paperdoll/poses/pose1_fishbot.png";
 
 const ClosetView = () => {
 
@@ -13,15 +14,24 @@ const ClosetView = () => {
     <div className="closet-view">
       <h1>Closet</h1>
 
-      {/* display user clothes testing */}
-      <ul>
-        {userClothes.map((clothing, index) => (
-          <li key={index}>
-            <p>{`ID: ${clothing.id}, User ID: ${clothing.user_id}, Clothing ID: ${clothing.clothing_article_id}`}</p>
-            <p>{`Created at: ${clothing.created_at}, Updated at: ${clothing.updated_at}`}</p>
-          </li>
-        ))}
-      </ul>
+
+      <div className="pose-container">
+
+        <img src={paperdoll} alt="Robot Paper Doll" className="paperdoll-image" />
+
+      </div>
+      <div className="dresser-container">
+        {/* display user clothes testing */}
+        <ul>
+          {userClothes.map((clothing, index) => (
+            <li key={index}>
+              <p>{`ID: ${clothing.id}, User ID: ${clothing.user_id}, Clothing ID: ${clothing.clothing_article_id}`}</p>
+              <p>{`Created at: ${clothing.created_at}, Updated at: ${clothing.updated_at}`}</p>
+            </li>
+          ))}
+        </ul>
+
+      </div>
 
 
       <Link to={'/cabin'}>
