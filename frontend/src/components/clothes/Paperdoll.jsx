0271c26215fc9fd0cbb66806e625e94671d5e8b2
OliverDanger <od.oliverdixon@gmail.com>
2023-11-01 02:12:37 -0400

    ReIntegrate react-svg for SVG rendering

--- frontend/src/components/clothes/Paperdoll.jsx
+++ frontend/src/components/clothes/Paperdoll.jsx
@@ -1,10 +1,27 @@
+import { ReactSVG } from 'react-svg';
+
 import '../../styles/clothes/Paperdoll.scss';
 import pose2_fishbot from '../../assets/paperdoll/poses/pose2_fishbot.svg';
 
 const Paperdoll = () => {
+  const handleSVGInjection = (svg) => {
+    const paths = svg.querySelectorAll('cls-1');
+    paths.forEach((path) => {
+      path.setAttribute('fill', 'blue');
+    });
+  };
+
+
   return (
     <div className="pose-container">
-      <img src={pose2_fishbot} alt="Robot Paper Doll" className="paperdoll-image" />
+      <ReactSVG
+        src={pose2_fishbot}
+        beforeInjection={(svg) => handleSVGInjection(svg)}
+        wrapper='span'
+        className='paperdoll-pose2'
+        style={{ width: 200 }}
+      />
+      {/* <img src={pose2_fishbot} alt="Robot Paper Doll" className="paperdoll-image" /> */}
     </div>
   );
 };
