0b13bd198e266af8af5aa29fe675edaa813c9399
OliverDanger <od.oliverdixon@gmail.com>
2023-11-01 18:52:18 -0400

    Add colors to clothes model in the backend and create corresponding CSS variables

--- backend/app/models/clothing_article.rb
+++ backend/app/models/clothing_article.rb
@@ -2,7 +2,7 @@ class ClothingArticle < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 500 }
-  validates :colour, presence: true, inclusion: { in: %w(peach cherry orange lemon lime pine sky ocean lavender grape cloud space slate chestnut), message: "%{value} is not a valid colour" }
+  validates :colour, presence: true, inclusion: { in: %w(peach watermelon cherry apple orange lemon lime pine sky ocean depths lavender grape cloud space slate bread toast), message: "%{value} is not a valid colour" }
   validates :fancy, numericality: true
   validates :cool, numericality: true
   validates :eccentric, numericality: true
--- frontend/src/components/clothes/Paperdoll.jsx
+++ frontend/src/components/clothes/Paperdoll.jsx
@@ -4,24 +4,14 @@ import '../../styles/clothes/Paperdoll.scss';
 import pose2_fishbot from '../../assets/paperdoll/poses/pose2_fishbot.svg';
 
 const Paperdoll = () => {
-  const handleSVGInjection = (svg) => {
-    const paths = svg.querySelectorAll('cls-1');
-    paths.forEach((path) => {
-      path.setAttribute('fill', 'blue');
-    });
-  };
-
 
   return (
     <div className="pose-container">
       <ReactSVG
         src={pose2_fishbot}
-        beforeInjection={(svg) => handleSVGInjection(svg)}
-        wrapper='span'
         className='paperdoll-pose2'
         style={{ width: 200 }}
       />
-      {/* <img src={pose2_fishbot} alt="Robot Paper Doll" className="paperdoll-image" /> */}
     </div>
   );
 };
