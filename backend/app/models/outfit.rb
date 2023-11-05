6c5bc7d7c4b3585797034ce046e513f1c3746c67
OliverDanger <od.oliverdixon@gmail.com>
2023-11-04 23:33:56 -0400

    remove clothing slot validation for outfits

--- backend/app/models/outfit.rb
+++ backend/app/models/outfit.rb
@@ -1,15 +1,6 @@
 class Outfit < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 500 }
-  validate :at_least_one_clothing_location
-
-  private
-
-  def at_least_one_clothing_location
-    unless hat || face_upper || face_lower || neck || body_upper || body_lower || arm_upper || arm_lower || hand_left || hand_right || jacket || legs_upper || legs_lower || socks || shoes
-      errors.add(:base, "At least one clothing item location must be true")
-    end
-  end
 
   has_many :clothing_articles
   has_many :users
