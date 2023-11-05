266a1cd3bd5510929634dc3bfae4bd717cae1f3c
OliverDanger <od.oliverdixon@gmail.com>
2023-11-04 23:32:06 -0400

    update clothing_article model to handle new clothing category validation

--- backend/app/models/clothing_article.rb
+++ backend/app/models/clothing_article.rb
@@ -3,19 +3,11 @@ class ClothingArticle < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :colour, presence: true, inclusion: { in: %w(peach watermelon cherry apple orange lemon lime pine sky ocean trench grape blueberry cloud space slate bread toast), message: "%{value} is not a valid colour" }
+  validates :category, presence: true, inclusion: { in: %w(hat glasses face neckwear shirt jacket wristwear gloves shorts pants socks shoes)}
   validates :fancy, numericality: true
   validates :cool, numericality: true
   validates :eccentric, numericality: true
   validates :camouflage, numericality: true
-  validate :at_least_one_clothing_location
-
-  private
-
-  def at_least_one_clothing_location
-    unless hat || face_upper || face_lower || neck || body_upper || body_lower || arm_upper || arm_lower || hand_left || hand_right || jacket || legs_upper || legs_lower || socks || shoes
-      errors.add(:base, "At least one clothing item location must be true")
-    end
-  end
 
   has_many :owned_articles
   has_many :users, through: :owned_articles
