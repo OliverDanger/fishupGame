126717ec0b95f436a90b37742d408f67a5782d94
OliverDanger <od.oliverdixon@gmail.com>
2023-11-23 22:48:23 -0500

    PaperdollClothingArticle conditionally renders accent svg if clothinga rticle accent colour is present

--- frontend/src/components/clothes/PaperdollClothingArticle.jsx
+++ frontend/src/components/clothes/PaperdollClothingArticle.jsx
@@ -9,6 +9,8 @@ const PaperdollClothingArticle = ({ poseNumber, clothingArticle }) => {
   const [svgContentAccent, setSvgContentAccent] = useState(null);
 
   useEffect(() => {
+    console.log('🍕', clothingArticle);
+    // get main svg
     import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}-01.svg`)
       .then(svg => {
         setSvgContentMain(svg.default);
@@ -16,23 +18,26 @@ const PaperdollClothingArticle = ({ poseNumber, clothingArticle }) => {
       .catch(error => {
         console.error('Error loading paperdoll clothing SVG:', error);
       });
-
-    import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_trim-01.svg`)
-      .then(svg => {
-        setSvgContentAccent(svg.default);
-      })
-      .catch(error => {
-        console.error('Error loading paperdoll accent SVG:', error);
-      });
+    // get accent svg if accent colour present
+    if (clothingArticle.accent_colour && clothingArticle.accent_colour.trim() !== "") {
+      console.log('🦑', clothingArticle.name, clothingArticle.accent_colour);
+      import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_trim-01.svg`)
+        .then(svg => {
+          setSvgContentAccent(svg.default);
+        })
+        .catch(error => {
+          console.error('Error loading paperdoll accent SVG:', error);
+        });
+    }
   }, [poseNumber, clothingArticle]);
 
 
   return (
     <div className={`paperdoll-clothing-frame clothing-type-${clothingArticle.category}`}>
-      {svgContentAccent &&
+      {svgContentAccent && clothingArticle.accent_colour &&
         <ReactSVG
           src={svgContentAccent}
-          className={`paperdoll-clothing-accent orange-fill`}
+          className={`paperdoll-clothing-accent ${clothingArticle.accent_colour}-fill`}
         />
       }
       {svgContentMain &&
