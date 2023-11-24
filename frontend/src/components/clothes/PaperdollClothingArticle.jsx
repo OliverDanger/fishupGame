d6cbac1e9f2515ad710f7abd9d590b7d7b187e81
OliverDanger <od.oliverdixon@gmail.com>
2023-11-24 18:41:42 -0500

    PaperdollClothingArticle component renders clothing svgss in 3 parts by their colours

--- frontend/src/components/clothes/PaperdollClothingArticle.jsx
+++ frontend/src/components/clothes/PaperdollClothingArticle.jsx
@@ -7,10 +7,11 @@ import { useState, useEffect } from 'react';
 const PaperdollClothingArticle = ({ poseNumber, clothingArticle }) => {
   const [svgPart1, setSvgPart1] = useState(null);
   const [svgPart2, setSvgPart2] = useState(null);
+  const [svgPart3, setSvgPart3] = useState(null);
 
-  // get main svg
+  // get main (part 1) svg
   useEffect(() => {
-    import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}-01.svg`)
+    import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_part1-01.svg`)
       .then(svg => {
         setSvgPart1(svg.default);
       })
@@ -19,34 +20,51 @@ const PaperdollClothingArticle = ({ poseNumber, clothingArticle }) => {
       });
   }, [poseNumber, clothingArticle]);
 
-  // get accent svg if accent colour present
+  // get part 2 svg if accent colour present
   useEffect(() => {
-    console.log('🍕', clothingArticle);
-    if (clothingArticle.accent_colour && clothingArticle.accent_colour.trim() !== "") {
-      console.log('🦑', clothingArticle.name, clothingArticle.accent_colour);
-      import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_trim-01.svg`)
+    if (clothingArticle.colour_part2 && clothingArticle.colour_part2.trim() !== "") {
+      import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_part2-01.svg`)
         .then(svg => {
           setSvgPart2(svg.default);
         })
         .catch(error => {
-          console.error('Error loading paperdoll accent SVG:', error);
+          console.error(`Error loading paperdoll ${clothingArticle.category} part 2 SVG:`, error);
         });
+    }
+  }, [poseNumber, clothingArticle]);
+
+  // get part 3 svg if accent colour present
+  useEffect(() => {
+    if(clothingArticle.colour_part3 && clothingArticle.colour_part3.trim() !== "") {
+      import(`../../assets/paperdoll/${clothingArticle.category}/pose${poseNumber}/${clothingArticle.img}_part3-01.svg`)
+        .then(svg => {
+          setSvgPart3(svg.default);
+        })
+        .catch(error => {
+          console.error(`Error loading paperdoll ${clothingArticle.category} part 3 SVG:`, error)
+        })
     }
   }, [poseNumber, clothingArticle]);
 
 
   return (
     <div className={`paperdoll-clothing-frame clothing-type-${clothingArticle.category}`}>
-      {svgContentAccent && clothingArticle.accent_colour &&
+      {svgPart3 && clothingArticle.colour_part3 &&
         <ReactSVG
-          src={svgPart2}
-          className={`paperdoll-clothing-accent ${clothingArticle.accent_colour}-fill`}
+          src={svgPart3}
+          className={`paperdoll-clothing-part3 ${clothingArticle.colour_part3}-fill`}
         />
       }
-      {svgContentMain &&
+      {svgPart2 && clothingArticle.colour_part2 &&
+        <ReactSVG
+          src={svgPart2}
+          className={`paperdoll-clothing-part2 ${clothingArticle.colour_part2}-fill`}
+        />
+      }
+      {svgPart1 &&
         <ReactSVG
           src={svgPart1}
-          className={`paperdoll-clothing-main ${clothingArticle.colour}-fill`}
+          className={`paperdoll-clothing-part1 ${clothingArticle.colour_part1}-fill`}
         />
       }
     </div>
