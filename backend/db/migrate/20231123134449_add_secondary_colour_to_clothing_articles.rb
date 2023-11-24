bbdb57348dad939029d645a264c47770fa59ee8a
OliverDanger <od.oliverdixon@gmail.com>
2023-11-23 21:57:50 -0500

    migration to add accent_colour column to clothing_articles table

--- backend/db/migrate/20231123134449_add_secondary_colour_to_clothing_articles.rb
+++ backend/db/migrate/20231123134449_add_secondary_colour_to_clothing_articles.rb
@@ -1,4 +1,5 @@
 class AddSecondaryColourToClothingArticles < ActiveRecord::Migration[7.0]
   def change
+    add_column :clothing_articles, :accent_colour, :string
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_174724) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_134449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_174724) do
     t.datetime "updated_at", null: false
     t.string "img"
     t.string "category"
+    t.string "accent_colour"
   end
 
   create_table "inventories", force: :cascade do |t|
