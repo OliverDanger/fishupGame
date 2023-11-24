df5793285f7b275d41f27ed9497ccf394cc68939
OliverDanger <od.oliverdixon@gmail.com>
2023-11-23 22:03:26 -0500

    ClothingArticle model validates presence and validity of secondary colour column

--- backend/app/models/clothing_article.rb
+++ backend/app/models/clothing_article.rb
@@ -1,8 +1,10 @@
 class ClothingArticle < ApplicationRecord
+  VALID_COLORS = %w(peach watermelon cherry apple orange lemon lime pine sky ocean trench grape blueberry cloud space slate bread toast)
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 500 }
-  validates :colour, presence: true, inclusion: { in: %w(peach watermelon cherry apple orange lemon lime pine sky ocean trench grape blueberry cloud space slate bread toast), message: "%{value} is not a valid colour" }
+  validates :colour, presence: true, inclusion: { in: VALID_COLORS, message: "%{value} is not a valid colour" }
+  validates :accent_colour, presence: true, inclusion: { in: VALID_COLORS, message: "%{value} is not a valid colour" }
   validates :category, presence: true, inclusion: { in: %w(hat glasses face neckwear shirt jacket wristwear gloves shorts pants socks shoes)}
   validates :fancy, numericality: true
   validates :cool, numericality: true
