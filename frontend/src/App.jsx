f38603d79d15ec9c66f8dcf9bd4ddbff73da8f06
OliverDanger <od.oliverdixon@gmail.com>
2023-12-08 15:41:47 -0500

    remove user state code from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './hooks/context/UserContext';
 import DraggableApp from './DraggableApp';
@@ -13,35 +12,24 @@ import CabinView from './components/pages/CabinView';
 import ClosetView from './components/pages/ClosetView';
 import BelowDeckView from './components/pages/BelowDeckView';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-      user: {}
-    };
-  }
-  render() {
-    return (
-      <div>
-        <UserProvider>
-          <Router>
-            <DraggableApp>
-              <Routes>
-                <Route path='/' element={<TitleView />} />
-                <Route path='/login' element={<LoginView />} />
-                <Route path='/signup' element={<SignUpView />} />
-                <Route path='/map' element={<MapView />} />
-                <Route path='/cabin' element={<CabinView />} />
-                <Route path='/cabin/closet' element={<ClosetView />} />
-                <Route path='/below' element={<BelowDeckView />} />
-              </Routes>
-            </DraggableApp>
-          </Router>
-        </UserProvider>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <UserProvider>
+      <Router>
+        <DraggableApp>
+          <Routes>
+            <Route path='/' element={<TitleView />} />
+            <Route path='/login' element={<LoginView />} />
+            <Route path='/signup' element={<SignUpView />} />
+            <Route path='/map' element={<MapView />} />
+            <Route path='/cabin' element={<CabinView />} />
+            <Route path='/cabin/closet' element={<ClosetView />} />
+            <Route path='/below' element={<BelowDeckView />} />
+          </Routes>
+        </DraggableApp>
+      </Router>
+    </UserProvider>
+  );
+};
 
 export default App;
