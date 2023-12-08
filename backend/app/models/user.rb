d6d913e270eb01c6bcf01e9872312659e15da242
OliverDanger <od.oliverdixon@gmail.com>
2023-12-08 13:06:23 -0500

    user model validates username uniqueness

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, length: { maximum: 36, minimum: 3}, allow_nil: false
+  validates :username, presence: true, uniqueness: true, length: { maximum: 36, minimum: 3}, allow_nil: false
   validates :password_digest, length: { minimum: 8 }
   validates :x_coordinate, numericality: { only_integer: true }
   validates :y_coordinate, numericality: { only_integer: true }
